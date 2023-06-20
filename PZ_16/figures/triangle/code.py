b6c1b8e88ff279cebeec14d585ed3e3c950f31bb
Lipkax <103584293+Lipkax@users.noreply.github.com>
2023-06-20 19:16:19 +0300

    commit

--- PZ_16/figures/triangle/code.py
+++ PZ_16/figures/triangle/code.py
@@ -1,20 +1,14 @@
-from math import sqrt
 a = 7
 b = 2
 c = 8
 
 
-"""Функция для вычисленя периметра треугольника"""
+def triangle_perimetr(a_in=a, b_in=b, c_in=c):
+    """Возвращает периметр треугольника"""
+    return a_in + b_in + c_in
 
 
-def triangle_perimeter(st_a=a, st_b=b, st_c=c):
-    return st_a + st_b + st_c
-
-
-"""Функция для вычисленя площади треугольника"""
-
-
-def triangle_area(st_a=a, st_b=b, st_c=c):
-    p = (st_a + st_b + st_c) / 2
-    return sqrt(p * (p - st_a) * (p - st_b) * (p - st_c))
-
+def triangle_area(a_in=a, b_in=b, c_in=c):
+    """Возвращает площадь треугольника"""
+    p = (a_in + b_in + c_in) * 0.5
+    return (p * (p - a_in) * (p - b_in) * (p - c_in)) ** 0.5
