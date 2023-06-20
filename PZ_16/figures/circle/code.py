26b4c42b006100c3585a1c377954dd15b42cd072
Lipkax <103584293+Lipkax@users.noreply.github.com>
2023-06-20 19:13:49 +0300

    commit

--- PZ_16/figures/circle/code.py
+++ PZ_16/figures/circle/code.py
@@ -2,15 +2,12 @@ from math import pi
 
 default_radius = 5
 
-"""Функция для вычисления длины окружности"""
+
+def circle_perimetr(radius=default_radius):
+    """Возвращает периметр круга"""
+    return 2 * pi * radius
 
 
-def circle_perimeter(r=default_radius):
-    return pi * r * 2
-
-
-"""Функция для вычисления площали круга"""
-
-
-def circle_area(r=default_radius):
-    return pi * r ** 2
+def circle_area(radius=default_radius):
+    """Возвращает площадь круга"""
+    return pi * radius ** 2
